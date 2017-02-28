b00de4abbb44b6af367c41b82094283234d074d3
jeffreybaquiran <mail@jeffreybaquiran.com>
2017-02-28 15:55:58 +0800

    jef

---
+++ Windows10/autoinstall.ps1
@@ -0,0 +1,81 @@
+Set-ExecutionPolicy RemoteSigned
+
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+### Browser
+
+choco install googlechrome
+
+choco install firefox
+
+### Browser Plugin
+
+choco install adobeair
+
+choco install flashplayerplugin
+
+choco install flashplayerppapi
+
+choco install flashplayeractivex
+
+choco install adobeshockwaveplayer
+
+### Plugin
+
+choco install jre8
+
+choco install javaruntime
+
+choco install jdk8
+
+choco install silverlight
+
+choco install dotnet3.5
+
+choco install dotnet4.0
+
+choco install dotnet4.5
+
+choco install dotnet4.6.1
+
+choco install vcredist2010
+
+choco install vcredist2013
+
+choco install vcredist2015
+
+### Utility
+
+choco install adobereader
+
+choco install 7zip.install
+
+choco install skype
+
+choco install windirstat
+
+choco install everything
+
+choco install windirstat
+
+choco install cdburnerxp
+
+choco install classic-shell
+
+### Cloud
+
+choco install dropbox
+
+choco install googledrive
+
+### Media
+
+choco install vlc
+
+choco install quicktime
+
+choco install itunes
+
+choco install audacity
+
+choco install cccp
