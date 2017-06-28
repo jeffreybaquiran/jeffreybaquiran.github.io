b6d462a21d2ffb10a5d8281d89f108e66303eaea
jeffreybaquiran <mail@jeffreybaquiran.com>
2017-06-28 14:46:08 +0800

    Update LinuxEnableRoot.sh

--- LinuxEnableRoot.sh
+++ LinuxEnableRoot.sh
@@ -4,9 +4,13 @@ sudo apt-get update -y
 
 sudo apt-get upgrade -y
 
+sudo apt-get install nano -y
+
 sudo apt-get install openssh-server -y
 
 sudo sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
+sudo apt-get install xorg lxde-core tightvncserver -y
 
 exit 0
 
